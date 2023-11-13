4e981ff7a58b289f58506d402c8d07e76917aaf3
niedson <niofernandodf@gmail.com>
2023-11-13 14:03:29 -0300

    :recycle: redirect slide

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 import { SideMenu } from "@/components/SideMenu"
+import { redirect } from 'next/navigation'
 
 export default function Login() {
+
+  redirect('https://www.canva.com/design/DAFzhEAEsJk/Ro-hQhhsBTJemdRvBTHTzA/edit?utm_content=DAFzhEAEsJk&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton')
+
   return (
     <>
       <SideMenu />
