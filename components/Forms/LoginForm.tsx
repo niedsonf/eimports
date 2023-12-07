a111d15308a8f748a9a2cb93f362d70424668daf
Nio <niofernandodf@gmail.com>
2023-12-06 22:12:12 -0300

    :recycle: login page

--- components/Forms/LoginForm.tsx
+++ components/Forms/LoginForm.tsx
@@ -3,10 +3,12 @@
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 import { zodResolver } from '@hookform/resolvers/zod'
+import { WebServer } from "@/services/WebServer"
+import * as argon2 from "argon2";
 
 const loginFormSchema = z.object({
-    userName: z.string(),
-    password: z.string(),
+    login: z.string().min(3, 'Usuário inválido'),
+    password: z.string().min(3, 'Senha inválida'),
 })
 
 type LoginFormInputs = z.infer<typeof loginFormSchema>
@@ -16,31 +18,56 @@ export function LoginForm() {
     const {
         register,
         handleSubmit,
-        formState: { isSubmitting },
+        formState: { isSubmitting, errors },
     } = useForm<LoginFormInputs>({
-        resolver: zodResolver(loginFormSchema)
+        resolver: zodResolver(loginFormSchema),
+        shouldFocusError: false,
     })
 
+    async function onSubmit(data: LoginFormInputs) {
+        console.log(data)
+        // try {
+        //     const res = await argon2.hash(data.password)
+        //     console.log(res)
+        // } catch (e) {
+        //     console.log(e)
+        // }
+        try {
+            const isValidLogin = await WebServer.Login({ login: data.login, password: data.password })
+            console.log(isValidLogin)
+        } catch (e) {
+            console.log(e)
+        }
+        return
+    }
 
     return (
         <form
             className="flex flex-col gap-4"
-            action=""
+            onSubmit={handleSubmit(onSubmit)}
         >
             <input
                 className="form-input"
                 placeholder="Email ou Usuário"
                 type="text"
-
+                maxLength={50}
+                required
+                {...register("login")}
             />
             <input
                 className="form-input"
                 placeholder="Senha"
-                type="text"
+                type="password"
+                maxLength={50}
+                required
+                {...register("password")}
             />
-            <button 
-            className="form-submit mt-4"
-            disabled={isSubmitting}
+            <span className="text-xs text-red-300">
+                {(errors.login || errors.password) && 'Credenciais inválidas, verifique os dados.'}
+            </span>
+            <button
+                className="form-submit mt-4"
+                disabled={isSubmitting}
             >Entrar</button>
         </form>
     )
